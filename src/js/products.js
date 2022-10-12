98df06802e540220b84a1a958db34caa8dc7c74d
Vadim Ulanovskiy <vadimulanovskiy@gmail.com>
2022-10-12 14:32:58 +0300

    Add scripts for modal windows

--- src/js/products.js
+++ src/js/products.js
@@ -1,15 +1,23 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector('[products-modal-open]'),
-      closeModalBtn: document.querySelector('[products-modal-close]'),
-      modal: document.querySelector('[products-modal]'),
-    };
-  
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle('is-hidden');
-    }
-  })();
-  
+  const refs = {
+    openModalBtn: document.querySelector('[products-modal-open]'),
+    closeModalBtn: document.querySelector('[products-modal-close]'),
+    modal: document.querySelector('[products-modal]'),
+  };
+
+  const body = document.getElementById('body');
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  refs.openModalBtn.addEventListener('click', locking);
+  refs.closeModalBtn.addEventListener('click', locking);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+
+  function locking() {
+    body.classList.toggle('locked');
+  }
+})();
