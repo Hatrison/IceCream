92390f1174c1be7844082cc57796eeb7ea1f01ef
Vadim Ulanovskiy <vadimulanovskiy@gmail.com>
2022-10-12 13:00:08 +0300

    Update buy now button

--- src/js/header-buy-now.js
+++ src/js/header-buy-now.js
@@ -1,15 +1,16 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector('[buy-now-modal-open]'),
-      closeModalBtn: document.querySelector('[buy-now-modal-close]'),
-      modal: document.querySelector('[buy-now-modal]'),
-    };
-  
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle('is-hidden');
-    }
-  })();
-  
+  const refs = {
+    openModalBtn: document.querySelector('[buy-now-modal-open]'),
+    openModalBtnMobile: document.querySelector('[buy-now-modal-open-mobile]'),
+    closeModalBtn: document.querySelector('[buy-now-modal-close]'),
+    modal: document.querySelector('[buy-now-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtnMobile.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
