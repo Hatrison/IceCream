5f668252f032087fe374701678d1914c91089a8b
Vadim Ulanovskiy <vadimulanovskiy@gmail.com>
2022-10-14 00:30:04 +0300

    Modfy scripts and fix bugs

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -3,26 +3,27 @@
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
 
+  const body = document.getElementById('body');
+
   const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
-
-    // const scrollLockMethod = !isMenuOpen
-    //   ? 'disableBodyScroll'
-    //   : 'enableBodyScroll';
-    // bodyScrollLock[scrollLockMethod](document.body);
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
 
-  // Close the mobile menu on wider screens if the device orientation changes
+  openMenuBtn.addEventListener('click', locking);
+  closeMenuBtn.addEventListener('click', locking);
+
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
-    // bodyScrollLock.enableBodyScroll(document.body);
   });
+
+  function locking() {
+    body.classList.toggle('locked');
+  }
 })();
